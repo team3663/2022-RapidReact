8613d669678a78387b955aa752a36ce976c5b5a7
DylanChi308 <76456393+DylanChi308@users.noreply.github.com>
2022-02-17 20:58:14 -0800

    Created Intake Subsytem and two button commands

---
+++ RapidReact/src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -0,0 +1,66 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
+import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMax.IdleMode;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+public class IntakeSubsystem extends SubsystemBase {
+
+  private CANSparkMax intakeMotor;
+  private double power = 0.4;
+  final DoubleSolenoid intakeSolenoid;
+  private boolean isOut;
+  private final int MOTOR_CURRENT_LIMIT = 25;
+
+  /** Creates a new instance of the Shooter subsystem. */
+  public IntakeSubsystem(int motor1CANId, int solonoidInCanId, int solonoidOutCanId) {
+
+    intakeMotor = new CANSparkMax(motor1CANId, MotorType.kBrushless);
+
+    intakeMotor.setIdleMode(IdleMode.kCoast);
+    intakeMotor.setSmartCurrentLimit(MOTOR_CURRENT_LIMIT);
+
+    intakeSolenoid = new DoubleSolenoid(PneumaticsModuleType.REVPH, solonoidInCanId, solonoidOutCanId);
+
+
+
+  }
+
+  @Override
+  public void periodic() { }
+
+  public void start() {
+    intakeSolenoid.set(Value.kForward);
+    if(isOut == true){
+      intakeMotor.set(power);
+    } 
+  }
+
+  public void stop() {
+    retract();
+    intakeMotor.set(0.0);
+  }
+
+  public void extend(){
+    intakeSolenoid.set(Value.kForward);
+    isOut = true;
+  }
+
+  public void retract(){
+    intakeSolenoid.set(Value.kReverse);
+    isOut = false;
+  }
+
+  public void spinBallIn(){
+    intakeMotor.set(power);
+  }
+
+  public void spinBallOut(){
+    intakeMotor.set(-power);
+  }
+}
