f67eaa975faa53e0635f3e7685edc4cb497c4dcb
Richard Elmore <richardelmore@users.noreply.github.com>
2022-02-18 16:38:33 -0800

    Fix id assignment in continuous mode.

--- RapidReact/src/main/java/frc/robot/subsystems/FeederSubsystem.java
+++ RapidReact/src/main/java/frc/robot/subsystems/FeederSubsystem.java
@@ -318,7 +318,7 @@ public class FeederSubsystem extends SubsystemBase {
     private class ContinuousMode extends FeedModeBase {
 
         private ContinuousMode() {
-            super(FeedMode.SHOOT_ONE);
+            super(FeedMode.CONTINUOUS);
         }
 
         @Override
