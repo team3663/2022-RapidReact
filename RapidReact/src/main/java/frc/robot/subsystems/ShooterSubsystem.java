80ffd5737837e2c290921abc3de0a1df6a30d611
CamdonCon <52018350+CamdonCon@users.noreply.github.com>
2022-02-18 18:41:48 -0800

    removed unused code

--- RapidReact/src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ RapidReact/src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -45,10 +45,6 @@ public class ShooterSubsystem extends SubsystemBase {
   private final int MAX_HOOD_ANGLE = 80; //THESE NUMBERS ARE ALL COMPLETELY IMAGINARY
   private final int MIN_HOOD_ANGLE = 25; //THESE NUMBERS ARE ALL COMPLETELY IMAGINARY
 
-  @SuppressWarnings("unused") private final double WHEEL_GEAR_RATIO_MULTIPLIER = 1; //THESE NUMBERS ARE ALL COMPLETELY IMAGINARY
-  @SuppressWarnings("unused") private final double ANGLE_GEAR_RATIO_MULTIPLIER = 1; //THESE NUMBERS ARE ALL COMPLETELY IMAGINARY
-  private final double ROTATIONS_PER_DEGREE = 5; //THESE NUMBERS ARE ALL COMPLETELY IMAGINARY
-
   static final double powerIncrement = 0.05; 
   public double power = 0.0;
 
