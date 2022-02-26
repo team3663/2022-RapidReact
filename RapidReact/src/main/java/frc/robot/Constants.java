74919dd236704ddeed17460771840f109a478f20
CamdonCon <52018350+CamdonCon@users.noreply.github.com>
2022-02-26 04:42:41 -0800

    added a hood to max

--- RapidReact/src/main/java/frc/robot/Constants.java
+++ RapidReact/src/main/java/frc/robot/Constants.java
@@ -15,34 +15,41 @@ package frc.robot;
 public final class Constants {
 
   // Swerve drivetrain physical constants
-  public static final double DRIVETRAIN_TRACKWIDTH_METERS = 0.5461;
-  public static final double DRIVETRAIN_WHEELBASE_METERS = 0.5461;
+  public static final double DRIVETRAIN_TRACKWIDTH_METERS = .6604; // 0.5461 for both
+  public static final double DRIVETRAIN_WHEELBASE_METERS = 0.6604;
   public static final double DRIVE_TRAIN_WHEEL_DIAMETER_METERS = 0.106325;
-  public static final double FRONT_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(297.7734375);
-  public static final double FRONT_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(23.291015625);
-  public static final double BACK_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(95.27343750000001);
-  public static final double BACK_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(310.60546875);
+  public static final double FRONT_LEFT_MODULE_STEER_OFFSET = Math.toRadians(0);//224.126953125
+  public static final double FRONT_RIGHT_MODULE_STEER_OFFSET = Math.toRadians(0);//27.078857421875
+  public static final double BACK_LEFT_MODULE_STEER_OFFSET = Math.toRadians(0);// 275.1566162109375
+  public static final double BACK_RIGHT_MODULE_STEER_OFFSET = Math.toRadians(0);//190.38397216796875 
 
   // Xbox Controllers Port Indexes
   public static final int DRIVE_CONTROLLER_PORT = 0;
   public static final int OPERATOR_CONTROLLER_PORT = 1;
   public static final int TEST_CONTROLLER_PORT = 2;
 
+  /*
+        Front Left to Front Right
+        Back Left to Front Left
+        Front Right to Back Right
+        Back Right to Back Left
+        */
+
   // CAN IDs
-  public static final int FRONT_LEFT_MODULE_DRIVE_MOTOR = 1;
-  public static final int FRONT_RIGHT_MODULE_DRIVE_MOTOR = 2;
-  public static final int BACK_LEFT_MODULE_DRIVE_MOTOR = 3;
-  public static final int BACK_RIGHT_MODULE_DRIVE_MOTOR = 4;
+  public static final int FRONT_LEFT_MODULE_DRIVE_MOTOR = 4; //1
+  public static final int FRONT_RIGHT_MODULE_DRIVE_MOTOR = 3; //2
+  public static final int BACK_LEFT_MODULE_DRIVE_MOTOR = 2; //3
+  public static final int BACK_RIGHT_MODULE_DRIVE_MOTOR = 1; //4
 
-  public static final int FRONT_LEFT_MODULE_STEER_MOTOR = 5;
-  public static final int FRONT_RIGHT_MODULE_STEER_MOTOR = 6;
-  public static final int BACK_LEFT_MODULE_STEER_MOTOR = 7;
-  public static final int BACK_RIGHT_MODULE_STEER_MOTOR = 8;
+  public static final int FRONT_LEFT_MODULE_STEER_MOTOR = 8; //5
+  public static final int FRONT_RIGHT_MODULE_STEER_MOTOR = 7; //6 
+  public static final int BACK_LEFT_MODULE_STEER_MOTOR = 6; //7
+  public static final int BACK_RIGHT_MODULE_STEER_MOTOR = 5; //8
 
-  public static final int FRONT_LEFT_MODULE_STEER_ENCODER = 9;
-  public static final int FRONT_RIGHT_MODULE_STEER_ENCODER = 10;
-  public static final int BACK_LEFT_MODULE_STEER_ENCODER = 11;
-  public static final int BACK_RIGHT_MODULE_STEER_ENCODER = 12;
+  public static final int FRONT_LEFT_MODULE_STEER_ENCODER = 12;//9;
+  public static final int FRONT_RIGHT_MODULE_STEER_ENCODER = 11;//10;
+  public static final int BACK_LEFT_MODULE_STEER_ENCODER = 10;//11;
+  public static final int BACK_RIGHT_MODULE_STEER_ENCODER = 9;//12
 
   public static final int DRIVETRAIN_PIGEON_ID = 13;
 
