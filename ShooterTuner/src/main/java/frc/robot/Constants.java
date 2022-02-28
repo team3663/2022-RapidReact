a197519d3935647fceba62928658f8290564c593
Richard Elmore <richardelmore@users.noreply.github.com>
2022-02-27 23:16:30 -0800

    Create project from prototypes

---
+++ ShooterTuner/src/main/java/frc/robot/Constants.java
@@ -0,0 +1,21 @@
+package frc.robot;
+
+/**
+ * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
+ * constants. This class should not be used for any other purpose. All constants should be declared
+ * globally (i.e. public static). Do not put anything functional in this class.
+ *
+ * <p>It is advised to statically import this class (or one of its inner classes) wherever the
+ * constants are needed, to reduce verbosity.
+ */
+public final class Constants {
+
+    // Xbox Controllers Port Indexes
+    public static final int DRIVE_CONTROLLER_ID = 0;
+    public static final int OPERATOR_CONTROLLER_ID = 1;
+    public static final int TEST_CONTROLLER_ID = 2;
+
+    // CAN IDs for our motor controllers
+    public static final int MOTOR_1_CAN_ID = 1;
+    public static final int MOTOR_2_CAN_ID = 2;
+}
---
+++ ShooterTuner/src/main/java/frc/robot/RobotContainer.java
@@ -0,0 +1,59 @@
+package frc.robot;
+
+import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.XboxController.Button;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+
+import static frc.robot.Constants.*;
+
+import frc.robot.commands.ExampleCommand;
+import frc.robot.subsystems.ExampleSubsystem;
+
+/**
+ * This class is where the bulk of the robot should be declared. Since Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
+ * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
+ * subsystems, commands, and button mappings) should be declared here.
+ */
+public class RobotContainer {
+
+  //
+  private final XboxController controller = new XboxController(DRIVE_CONTROLLER_ID);
+
+  // Subsystems
+  private final ExampleSubsystem mySubsystem = new ExampleSubsystem();
+
+  // Commands
+  private final ExampleCommand autoCmd = new ExampleCommand(mySubsystem);
+
+  /** The container for the robot. Contains subsystems, OI devices, and commands. */
+  public RobotContainer() {
+    // Configure the button bindings
+    configureButtonBindings();
+  }
+
+  /**
+   * Use this method to define your button->command mappings. Buttons can be created by
+   * instantiating a {@link GenericHID} or one of its subclasses ({@link
+   * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a {@link
+   * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
+   */
+  private void configureButtonBindings() {
+
+    // Bind an InstantCommand that calls the doSomething method of our subsystem when button A is pressed.
+    new JoystickButton(controller, Button.kA.value).whenPressed(new InstantCommand(() -> mySubsystem.doSomething(), mySubsystem));
+  }
+
+  /**
+   * Use this to pass the autonomous command to the main {@link Robot} class.
+   *
+   * @return the command to run in autonomous
+   */
+  public Command getAutonomousCommand() {
+    // An ExampleCommand will run in autonomous
+    return autoCmd;
+  }
+}
