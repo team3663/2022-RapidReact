bb65120e3d7bfcaf8572a67ae621c7d311b58565
frankwyang <frank.yang@cedarpark.org>
2022-03-24 19:56:41 -0700

    CAN ID's for climber motors

--- RapidReact/src/main/java/frc/robot/Constants.java
+++ RapidReact/src/main/java/frc/robot/Constants.java
@@ -14,71 +14,78 @@ package frc.robot;
  */
 public final class Constants {
 
-  // Swerve drivetrain physical constants
-  public static final double DRIVETRAIN_TRACKWIDTH_METERS = .6604; // 0.5461 for both
-  public static final double DRIVETRAIN_WHEELBASE_METERS = 0.6604;
-  public static final double DRIVE_TRAIN_WHEEL_DIAMETER_METERS = 0.106325;
-  public static final double FRONT_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(64.764404296875);//297.7734375
-  public static final double FRONT_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(261.749267578125);//23.291015625
-  public static final double BACK_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(11.865234375);//95.27343750000001
-  public static final double BACK_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(94.482421875);//310.60546875
+	// Swerve drivetrain physical constants
+	public static final double DRIVETRAIN_TRACKWIDTH_METERS = .6604; // 0.5461 for both
+	public static final double DRIVETRAIN_WHEELBASE_METERS = 0.6604;
+	public static final double DRIVE_TRAIN_WHEEL_DIAMETER_METERS = 0.106325;
+	public static final double FRONT_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(64.764404296875);// 297.7734375
+	public static final double FRONT_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(261.749267578125);// 23.291015625
+	public static final double BACK_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(11.865234375);// 95.27343750000001
+	public static final double BACK_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(94.482421875);// 310.60546875
 
-  // Limelight
-  public static final double CAMERA_ANGLE = 36;
-  public static final double CAMERA_HEIGHT = 0.5842;
-  public static final double TARGET_HEIGHT = 2.6414;
+	// Limelight
+	public static final double CAMERA_ANGLE = 36;
+	public static final double CAMERA_HEIGHT = 0.5842;
+	public static final double TARGET_HEIGHT = 2.6414;
 
-  // Xbox Controllers Port Indexes
-  public static final int DRIVE_CONTROLLER_PORT = 0;
-  public static final int OPERATOR_CONTROLLER_PORT = 1;
-  public static final int TEST_CONTROLLER_PORT = 2;
+	// Xbox Controllers Port Indexes
+	public static final int DRIVE_CONTROLLER_PORT = 0;
+	public static final int OPERATOR_CONTROLLER_PORT = 1;
+	public static final int TEST_CONTROLLER_PORT = 2;
 
-  /*
-   * Front Left to Front Right
-   * Back Left to Front Left
-   * Front Right to Back Right
-   * Back Right to Back Left
-   */
+	/*
+	 * Front Left to Front Right
+	 * Back Left to Front Left
+	 * Front Right to Back Right
+	 * Back Right to Back Left
+	 */
 
-  // CAN IDs
-  public static final int FRONT_LEFT_MODULE_DRIVE_MOTOR = 1; // 1
-  public static final int FRONT_RIGHT_MODULE_DRIVE_MOTOR = 2; // 2
-  public static final int BACK_LEFT_MODULE_DRIVE_MOTOR = 3; // 3
-  public static final int BACK_RIGHT_MODULE_DRIVE_MOTOR = 4; // 4
+	// CAN IDs
+	public static final int FRONT_LEFT_MODULE_DRIVE_MOTOR = 1; 
+	public static final int FRONT_RIGHT_MODULE_DRIVE_MOTOR = 2; 
+	public static final int BACK_LEFT_MODULE_DRIVE_MOTOR = 3; 
+	public static final int BACK_RIGHT_MODULE_DRIVE_MOTOR = 4; 
 
-  public static final int FRONT_LEFT_MODULE_STEER_MOTOR = 5; // 5
-  public static final int FRONT_RIGHT_MODULE_STEER_MOTOR = 6; // 6
-  public static final int BACK_LEFT_MODULE_STEER_MOTOR = 7; // 7
-  public static final int BACK_RIGHT_MODULE_STEER_MOTOR = 8; // 8
+	public static final int FRONT_LEFT_MODULE_STEER_MOTOR = 5; 
+	public static final int FRONT_RIGHT_MODULE_STEER_MOTOR = 6; 
+	public static final int BACK_LEFT_MODULE_STEER_MOTOR = 7; 
+	public static final int BACK_RIGHT_MODULE_STEER_MOTOR = 8; 
 
-  public static final int FRONT_LEFT_MODULE_STEER_ENCODER = 9;// 9;
-  public static final int FRONT_RIGHT_MODULE_STEER_ENCODER = 10;// 10;
-  public static final int BACK_LEFT_MODULE_STEER_ENCODER = 11;// 11;
-  public static final int BACK_RIGHT_MODULE_STEER_ENCODER = 12;// 12
+	public static final int FRONT_LEFT_MODULE_STEER_ENCODER = 9;
+	public static final int FRONT_RIGHT_MODULE_STEER_ENCODER = 10;
+	public static final int BACK_LEFT_MODULE_STEER_ENCODER = 11;
+	public static final int BACK_RIGHT_MODULE_STEER_ENCODER = 12;
 
-  public static final int DRIVETRAIN_PIGEON_ID = 13;
+	public static final int DRIVETRAIN_PIGEON_ID = 13;
 
-  public static final int INTAKE_MOTOR_CAN_ID = 15;
+	public static final int INTAKE_MOTOR_CAN_ID = 15;
 
-  public static final int FEEDER_MOTOR_CAN_ID = 20;
+	public static final int FEEDER_MOTOR_CAN_ID = 20;
 
-  public static final int SHOOTER_MOTOR_1_CAN_ID = 25;
-  public static final int SHOOTER_MOTOR_2_CAN_ID = 26;
-  public static final int HOOD_MOTOR_CAN_ID = 27;
+	public static final int SHOOTER_MOTOR_1_CAN_ID = 25;
+	public static final int SHOOTER_MOTOR_2_CAN_ID = 26;
+	public static final int HOOD_MOTOR_CAN_ID = 27;
 
-  // Solenoid Channels
-  public static final int ARM_RETRACT_SOLENOID_CHAN = 0;
-  public static final int ARM_EXTEND_SOLENOID_CHAN = 1;
-  public static final int BOOM_RETRACT_SOLENOID_CHAN = 2;
-  public static final int BOOM_EXTEND_SOLENOID_CHAN = 3;
+	// Climber motors
+	public static final int ELEVATOR_CAN_ID = 30;
+	public static final int WINDMILL_1_CAN_ID = 31;
+	public static final int WINDMILL_2_CAN_ID = 32;
+	public static final int RED_HOOK_CAN_ID = 33;
+	public static final int BLUE_HOOK_CAN_ID = 34;
 
-  // Digital IO ports
-  public static final int FEEDER_ENTRY_SENSOR_DIO = 1;
-  public static final int FEEDER_EXIT_SENSOR_DIO = 2;
-  public static final int HOOD_LIMITSWITCH_DIO = 3;
+	// Solenoid Channels
+	public static final int ARM_RETRACT_SOLENOID_CHAN = 0;
+	public static final int ARM_EXTEND_SOLENOID_CHAN = 1;
+	public static final int BOOM_RETRACT_SOLENOID_CHAN = 2;
+	public static final int BOOM_EXTEND_SOLENOID_CHAN = 3;
 
-  // Analog IO ports
+	// Digital IO ports
+	public static final int FEEDER_ENTRY_SENSOR_DIO = 1;
+	public static final int FEEDER_EXIT_SENSOR_DIO = 2;
+	public static final int HOOD_LIMITSWITCH_DIO = 3;
 
-  // SPI addresses
+	// Analog IO ports
+
+	// SPI addresses
 
 }
