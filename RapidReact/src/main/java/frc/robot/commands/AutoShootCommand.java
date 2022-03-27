0d00d1feb13a9be99cf4413bb481d7eab906619f
Yvonne Guan <yvonneguan70@gmail.com>
2022-03-27 16:08:19 -0700

    combine auto shoot and wait hood

--- RapidReact/src/main/java/frc/robot/commands/AutoShootCommand.java
+++ RapidReact/src/main/java/frc/robot/commands/AutoShootCommand.java
@@ -72,7 +72,7 @@ public class AutoShootCommand extends CommandBase {
         }
 
         // We only get here if cargo staging has completed.
-        if (shooter.ready() && limelight.aligned()) { 
+        if (shooter.ready() && limelight.aligned() && shooter.available()) { 
             feeder.setFeedMode(FeedMode.CONTINUOUS);
         }
         else {
