81312d85010bd28cb0f0dac288d012687f4cb486
Richard Elmore <richardelmore@users.noreply.github.com>
2022-04-03 17:39:59 -0700

    Remove unused code

--- RapidReact/src/main/java/frc/robot/RobotContainer.java
+++ RapidReact/src/main/java/frc/robot/RobotContainer.java
@@ -2,7 +2,6 @@ package frc.robot;
 
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.XboxController.Button;
 import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
@@ -12,7 +11,6 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
-import edu.wpi.first.wpilibj2.command.button.POVButton;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.commands.AutoAlignWithHubCommand;
@@ -22,16 +20,13 @@ import frc.robot.commands.DefaultDriveCommand;
 import frc.robot.commands.DefaultShooterCommand;
 import frc.robot.commands.ExtendElevatorCommand;
 import frc.robot.commands.HomeElevatorCommand;
-import frc.robot.commands.IncrementWindmillAngle;
 import frc.robot.commands.FollowerCommand;
 import frc.robot.commands.IntakeCommand;
-import frc.robot.commands.MoveWindmillCommand;
 import frc.robot.commands.RotateWindmillCommand;
 import frc.robot.commands.ShootCommand;
 import frc.robot.commands.SwitchBlueHookCommand;
 import frc.robot.commands.SwitchRedHookCommand;
 import frc.robot.commands.WaitForSecondsCommand;
-import frc.robot.commands.WaitShooterAvailableCommand;
 import frc.robot.commands.AutoIntakeCommand.IntakeMode;
 import frc.robot.drivers.Pigeon;
 import frc.robot.subsystems.ClimberSubsystem;
@@ -81,7 +76,6 @@ public class RobotContainer {
     private ClimberSubsystem climber;
 
     // Commands
-    private DefaultDriveCommand drive;
     private Command deployClimberCommand;
     private Command climbCommand;
     private Command climbSecondToThirdCommmand;
@@ -355,17 +349,17 @@ public class RobotContainer {
         return new SequentialCommandGroup(createShootOnlyCommand(), createTaxiOnlyCommand());
     }
 
-    private Command createLeftTwoBallCommand() {
+/*     private Command createLeftTwoBallCommand() {
         return new SequentialCommandGroup(
             new InstantCommand(() -> drivetrain.resetPosition()),
             new InstantCommand(() -> shooter.idle()),
             new AutoIntakeCommand(intake, feeder, IntakeMode.extended),
             new FollowerCommand(drivetrain, TrajectoryFactory.twoMetersForward),
             new ParallelCommandGroup(
-                new AutoAlignWithHubCommand(limelight, drivetrain),
-                new AutoShootCommand(shooter, feeder, limelight, 2, false)),
+            new AutoAlignWithHubCommand(limelight, drivetrain),
+            new AutoShootCommand(shooter, feeder, limelight, 2, false)),
             new AutoIntakeCommand(intake, feeder, IntakeMode.retracted));
-    }
+    } */
 
     public Command createRightTwoBallCommand() {
         return new SequentialCommandGroup(new InstantCommand(() -> shooter.idle()),
