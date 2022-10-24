d64272de867ec4e9cbb9f5693da2a0ad88c36764
Jacob Bublitz <jacob@bublitz.com>
2022-10-23 20:06:38 -0700

    Limit acceleration for teleop

--- RapidReact/src/main/java/frc/robot/commands/DefaultDriveCommand.java
+++ RapidReact/src/main/java/frc/robot/commands/DefaultDriveCommand.java
@@ -3,15 +3,23 @@ package frc.robot.commands;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.DrivetrainSubsystem;
+import edu.wpi.first.math.filter.SlewRateLimiter;
 
 import java.util.function.DoubleSupplier;
 
 public class DefaultDriveCommand extends CommandBase {
+    private static final double MAX_LINEAR_ACCELERATION = 15.0;
+    private static final double MAX_ANGULAR_ACCELERATION = Math.toDegrees(2.5);
+
     private final DrivetrainSubsystem drivetrainSubsystem;
 
     private final DoubleSupplier translationXSupplier;
     private final DoubleSupplier translationYSupplier;
     private final DoubleSupplier rotationSupplier;
+
+    private final SlewRateLimiter xRateLimiter = new SlewRateLimiter(MAX_LINEAR_ACCELERATION);
+    private final SlewRateLimiter yRateLimiter = new SlewRateLimiter(MAX_LINEAR_ACCELERATION);
+    private final SlewRateLimiter rotationRateLimiter = new SlewRateLimiter(MAX_ANGULAR_ACCELERATION);
 
     public DefaultDriveCommand(DrivetrainSubsystem drivetrainSubsystem,
                                DoubleSupplier translationXSupplier,
@@ -28,15 +36,19 @@ public class DefaultDriveCommand extends CommandBase {
     @Override
     public void initialize() {
         //drivetrainSubsystem.resetPosition();
+	
+	xRateLimiter.reset(translationXSupplier.getAsDouble());
+	yRateLimiter.reset(translationYSupplier.getAsDouble());
+	rotationRateLimiter.reset(rotationSupplier.getAsDouble());
     }
 
     @Override
     public void execute() {
         drivetrainSubsystem.drive(
                 ChassisSpeeds.fromFieldRelativeSpeeds(
-                        translationXSupplier.getAsDouble(),
-                        translationYSupplier.getAsDouble(),
-                        rotationSupplier.getAsDouble(),
+                        xRateLimiter.calculate(translationXSupplier.getAsDouble()),
+                        yRateLimiter.calculate(translationYSupplier.getAsDouble()),
+                        rotationRateLimiter.calculate(rotationSupplier.getAsDouble()),
                         drivetrainSubsystem.getPose().getRotation()
                 ));
     }
